494a151432dd72be765fc989c8e0fa5f0b9d8a62
Alex Sieke <asieke@gmail.com>
2023-01-30 23:22:45 -0500

    added sound and matching logic

--- components/Card.tsx
+++ components/Card.tsx
@@ -3,27 +3,27 @@ import React, { useState, useEffect } from 'react';
 //create type for props
 type CardProps = {
   value: string;
+  flipped: boolean;
+  handler: () => void;
 };
 
-const Card = ({ value }: CardProps) => {
-  const [flipped, setFlipped] = useState(false);
+const Card = ({ value, flipped, handler }: CardProps) => {
+  // const flip = () => {
+  //   setFlipped(!flipped);
+  //   var msg = new SpeechSynthesisUtterance();
+  //   msg.text = value;
 
-  const flip = () => {
-    setFlipped(!flipped);
-    var msg = new SpeechSynthesisUtterance();
-    msg.text = value;
-
-    const audio = new Audio('/sounds/flip.mp3');
-    audio.play();
-    //wait for audio to finish playing
-    setTimeout(() => {
-      window.speechSynthesis.speak(msg);
-    }, 200);
-  };
+  //   const audio = new Audio('/sounds/flip.mp3');
+  //   audio.play();
+  //   //wait for audio to finish playing
+  //   setTimeout(() => {
+  //     window.speechSynthesis.speak(msg);
+  //   }, 200);
+  // };
 
   return (
-    <div className='flip'>
-      <div className={(flipped ? ' flip-clicked' : '') + ' flip-content'} onClick={flip}>
+    <div className='flip' onClick={handler}>
+      <div className={(flipped ? ' flip-clicked' : '') + ' flip-content'}>
         <div className='flip-front'>Front</div>
         <div className='flip-back'>
           <strong>{value}</strong>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,94 @@
 import Card from '../components/Card';
 import React, { useState, useEffect } from 'react';
 
+interface Status {
+  [key: number]: number;
+}
+
 export default function Home() {
-  //generate an array with 16 random animals
-
   //set initial state of animals to an empty array
-
-  const [animals, setAnimals] = useState([] as string[]);
+  const [cards, setCards] = useState([] as string[]);
+  const [status, setStatus] = useState<Status>({});
 
   //use effect, setAnimals to a shuffled array of animals
   useEffect(() => {
     const seed = ['Lion', 'Tiger', 'Elephant', 'Giraffe', 'Monkey', 'Kangaroo', 'Panda', 'Zebra'];
-    setAnimals([...seed, ...seed].sort(() => Math.random() - 0.5));
+    //create an array with 16 0s
+
+    setCards([...seed, ...seed].sort(() => Math.random() - 0.5));
   }, []);
 
-  console.log(animals);
+  const handleClick = (i: number) => {
+    if (status[i] === 1 || status[i] === 2) {
+      return;
+    }
+
+    // count the number of elements in status where status is 1
+    // if there are already two cards flipped, don't flip any more
+    const count = Object.values(status).filter((x) => x === 1).length;
+    if (count === 2) {
+      return;
+    }
+
+    playCardFlipSound(cards[i]);
+
+    // iterate through statuses and find the value where status is 1
+    let flipped: number = -1;
+    for (let j in status) {
+      if (status[j] === 1) {
+        flipped = parseInt(j);
+        break;
+      }
+    }
+
+    // first card to be flipped
+    if (flipped === -1) {
+      setStatus({ ...status, [i]: 1 });
+    } else {
+      //its a match!
+      if (cards[i] === cards[flipped]) {
+        setTimeout(() => {
+          playSound('match');
+        }, 1000);
+        setStatus({ ...status, [i]: 2, [flipped]: 2 });
+      } else {
+        setStatus({ ...status, [i]: 1 });
+        setTimeout(() => {
+          playSound('flip');
+          setStatus({ ...status, [i]: 0, [flipped]: 0 });
+        }, 2000);
+      }
+    }
+  };
+
+  console.log(status);
 
   return (
     <>
       <h1 className='text-4xl text-center font-semibold m-10'>Matching Game</h1>
       <div className='h-4/5 w-4/5 mx-auto grid grid-cols-4 gap-4'>
-        {animals.map((x, i) => (
+        {cards.map((x, i) => (
           <div key={i}>
-            <Card value={x} />
+            <Card value={x} flipped={!!status[i]} handler={() => handleClick(i)} />
           </div>
         ))}
       </div>
     </>
   );
 }
+
+const playCardFlipSound = (value: string) => {
+  var msg = new SpeechSynthesisUtterance();
+  msg.text = value;
+
+  playSound('flip');
+  //wait for audio to finish playing
+  setTimeout(() => {
+    window.speechSynthesis.speak(msg);
+  }, 200);
+};
+
+const playSound = (sound: string) => {
+  const audio = new Audio(`/sounds/${sound}.mp3`);
+  audio.play();
+};
